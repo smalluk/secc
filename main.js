7d996865fb47d9eec947f1baf63378e972361018
Daniel Midgley <daniel.midgley@aviva.com>
2025-03-18 15:39:18 +0000

    updated main page to have alternating photos in the hero section

--- main.js
+++ main.js
@@ -20,3 +20,37 @@ const slider = new A11YSlider(document.querySelector(".slider"), {
     },
   },
 });
+
+// Array of image URLs
+const images = [
+  'images/1.jpg',
+  'images/2.jpg',
+  'images/3.jpg',
+  'images/4.jpg',
+  'images/5.jpg',
+  'images/6.jpg',
+  'images/7.jpg',
+  'images/8.jpg',
+  'images/9.jpg',
+  'images/10.jpg',
+  'images/11.jpg',
+  'images/12.jpg',
+  'images/13.jpg',
+  'images/14.jpg',
+  'images/15.jpg',
+  'images/16.jpg',
+  'images/17.jpg',
+  'images/18.jpg',
+  'images/19.jpg',
+  'images/20.jpg',
+  'images/21.jpg'
+];
+
+// Function to select a random image
+function getRandomImage() {
+  const randomIndex = Math.floor(Math.random() * images.length);
+  return images[randomIndex];
+}
+
+// Set the src attribute of the image element
+document.getElementById('randomImage').src = getRandomImage();
